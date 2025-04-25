9c220acb9716ae216447e48b457517e0829868b6
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-25 20:19:17 +0300

    L-231, 232. Use fetch to load places data from backend

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // TODO ANBOL this creates an infinite loop. Render -> fetch -> setState -> render -> fetch -> setState -> [...].
+  fetch('http://localhost:3000/places')
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseData) => {
+      setAvailablePlaces(responseData.places);
+    });
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
