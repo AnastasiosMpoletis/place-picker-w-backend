3640c757ddc5a857702738eadb032c65cdff03bd
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-25 20:35:01 +0300

    L-233. Wrap fetching of available places with useEffect to avoid infinite loop

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  // TODO ANBOL this creates an infinite loop. Render -> fetch -> setState -> render -> fetch -> setState -> [...].
-  fetch('http://localhost:3000/places')
-    .then((response) => {
-      return response.json();
-    })
-    .then((responseData) => {
-      setAvailablePlaces(responseData.places);
-    });
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setAvailablePlaces(responseData.places);
+      });
+  }, [])
 
   return (
     <Places
