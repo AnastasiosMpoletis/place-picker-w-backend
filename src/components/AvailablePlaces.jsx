893981d1afefe208a33340a1030e75d874cc29d9
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-26 16:41:39 +0300

    L-236. Handle fetch error by rendering an Error component

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,9 +1,17 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  /**
+   * Common fetching states practise:
+   * 1. Loading state
+   * 2. Data state
+   * 3. Error state
+   */
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     /**
@@ -12,14 +20,38 @@ export default function AvailablePlaces({ onSelectPlace }) {
      */
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const responseData = await response.json();
-      setAvailablePlaces(responseData.places);
+
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const responseData = await response.json();
+
+        /**
+         * ok = 200 or 300 codes.
+         * !ok = 400 or 500 codes.
+         */
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+
+        setAvailablePlaces(responseData.places);
+      } catch (error) {
+        setError({ message: error.message || 'Could not fetch places, please try again later.' });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, [])
+
+  if (error) {
+    return (
+      <Error
+        title="An error occured!"
+        message={error.message}
+      />
+    );
+  }
 
   return (
     <Places
