8c5a169ca37e74e43af071244030edb93dffa5bc
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-26 17:58:14 +0300

    L-240. Implement updating user places

--- src/http.js
+++ src/http.js
@@ -1,6 +1,3 @@
-/**
- * @returns places from backend
- */
 export async function fetchAvailablePlaces() {
     const response = await fetch('http://localhost:3000/places');
     const responseData = await response.json();
@@ -14,4 +11,22 @@ export async function fetchAvailablePlaces() {
     }
 
     return responseData.places;
+}
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places }), // places = places: places shortcut
+        headers: {
+            'Content-Type': 'application/json', // inform requestthat  data attached to this request (body) is in JSON format
+        }
+    });
+
+    const responseData = await response.json();
+
+    if (!ok) {
+        throw new Error('Failed to update user data.');
+    }
+
+    return responseData.message;
 }
