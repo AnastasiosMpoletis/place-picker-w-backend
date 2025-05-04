5c26b960c9e9b6406cf3aa0b192bf7217911809a
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-05-04 12:24:55 +0300

    L-249, 250. Refactor fetchPlaces in AvailablePlaces component to useFetch custom hook. Implement sorting with fetchSortedPlaces

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,8 +1,27 @@
-import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js'
 import { fetchAvailablePlaces } from '../http.js'
+import { useFetch } from '../hooks/useFetch.js';
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  /**
+   * Promise has a reject function also: new Promise((resolve, reject) => {}).
+   */
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 
 export default function AvailablePlaces({ onSelectPlace }) {
@@ -12,40 +31,15 @@ export default function AvailablePlaces({ onSelectPlace }) {
    * 2. Data state
    * 3. Error state
    */
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [availablePlaces, setAvailablePlaces] = useState([]);
+  // const [error, setError] = useState();
 
-  useEffect(() => {
-    /**
-     * We can use async, await keywords to make an async function.
-     * This way, rendering will wait for the data loading to complete.
-     */
-    async function fetchPlaces() {
-      setIsFetching(true);
-
-      try {
-        const places = await fetchAvailablePlaces();
-
-        // Function will be executed once the position is loaded.
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
-          setAvailablePlaces(sortedPlaces);
-          /**
-           * Move setIsFetching from previous position because JavaScript does not wait for the code to execute before this callback function is done.
-           * Also notice setIsFetching is catch.
-           */
-          setIsFetching(false);
-        });
-
-      } catch (error) {
-        setError({ message: error.message || 'Could not fetch places, please try again later.' });
-        setIsFetching(false);
-      }
-    }
-
-    fetchPlaces();
-  }, [])
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return (
